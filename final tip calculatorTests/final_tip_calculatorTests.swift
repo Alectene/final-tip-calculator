14c27bc22dff92463a6af7cd04e01402ce75c42c
Alec Tenefrancia <alectenefrancia@alecs-macbook-air.local>
2019-01-30 23:19:05 -0800

    Initial Commit

---
+++ final tip calculatorTests/final_tip_calculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  final_tip_calculatorTests.swift
+//  final tip calculatorTests
+//
+//  Created by Alec Tenefrancia on 1/30/19.
+//  Copyright © 2019 Alec Tenefrancia. All rights reserved.
+//
+
+import XCTest
+@testable import final_tip_calculator
+
+class final_tip_calculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
